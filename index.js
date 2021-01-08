447a9c3589c04ad04cb01909b6ac7d55372fcf77
iamchenjiajun <iamchenjiajun@gmail.com>
2021-01-08 17:50:14 +0800

    change default file on root

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let rooms = {}; // {"room_id":"number of people"}
 //set up server
 const server = express()
     .use(express.static('./'))
-    .use((req, res) => res.sendFile("./app/minesweeper_index.html", { root: __dirname }))
+    .use((req, res) => res.sendFile("./app/index.html", { root: __dirname }))
     .use(express.static(__dirname + '/static'))
     .listen(PORT, () => console.log(`listening on ${PORT}`))
 
