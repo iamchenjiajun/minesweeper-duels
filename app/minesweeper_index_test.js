7714d1e0163656458cd2f22f1234f1bd08ea1095
iamchenjiajun <iamchenjiajun@gmail.com>
2021-01-08 16:33:23 +0800

    add populating squares

--- app/minesweeper_index_test.js
+++ app/minesweeper_index_test.js
@@ -1,35 +1,70 @@
 board_length = 16
 bomb_number = 40
 
-//making board
-array2d = new Array(board_length)
-for (i = 0; i < board_length; i++) {
-    array2d[i] = new Array(board_length);
-}
-console.log(array2d.length);
-console.log(Math.random());
-
-//putting bombs
-//-1 = bomb
-for (i = 0; i < bomb_number; i++) {
-    row = [Math.floor(Math.random() * 16)]; //find place to put bomb, if already got bomb, find again
-    col = [Math.floor(Math.random() * 16)];
-    while (array2d[row][col] == 1) {
-        row = [Math.floor(Math.random() * 16)];
-        col = [Math.floor(Math.random() * 16)];
-    }
-    array2d[row][col] = -1;
-}
-
-//creating the 16x16 buttons
-var x = document.getElementById("lobby");
-for (i = 0; i < board_length; i++) {
-    var row = document.createElement("div");
-    x.appendChild(row);
-    for (j = 0; j < board_length; j++) {
-        var button = document.createElement('BUTTON');
-        button.textContent = 'O';
-        row.appendChild(button);
+// struct to hold squares
+class Square {
+    constructor() {
+        this.isMine = false;
+        this.isFlagged = false;
+        this.neighbourCount = 0;
     }
 }
 
+// creates a 2d array
+function create_array2d(rows, cols) {
+    let array2d = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+        array2d[i] = new Array(cols);
+    }
+    return array2d;
+}
+
+// populates the array with Square objects
+function populate_array2d(array2d, rows, cols) {
+    for (let i=0; i < rows; i++) {
+        for (let j=0; j < cols; j++) {
+            array2d[i][j] = new Square();
+            array2d[i][j].isMine = (Math.random() > 0.5) ? true: false; // 50% chance to be a mine
+        }
+    }
+
+    for (let i=0; i < rows; i++) {
+        for (let j=0; j< cols; j++) {
+            array2d[i][j].neighbourCount = get_neighbour_count(array2d, i, j, rows, cols);
+        }
+    }
+}
+
+function get_neighbour_count(array2d, row, col, rows, cols) {
+    let count = 0;
+    for (let i=-1; i<=1; i++) {
+        for (let j=-1; j<=1; j++) {
+            if (row+i >= rows || col+j >= cols || row+i <0 || col+j <0) continue;
+            if (i==0 && j==0 ) continue;
+            if (array2d[row+i][col+j].isMine) count++;
+        }
+    }
+    return count;
+}
+
+function render(array2d) {
+    //creating the 16x16 buttons
+    var x = document.getElementById("game");
+    for (i = 0; i < board_length; i++) {
+        var row = document.createElement("div");
+        x.appendChild(row);
+        for (j = 0; j < board_length; j++) {
+            var button = document.createElement('BUTTON');
+            if (array2d[i][j].isMine) {
+                button.textContent = 'X';
+            } else {
+                button.textContent = array2d[i][j].neighbourCount;
+            }
+            row.appendChild(button);
+        }
+    }
+}
+
+let array2d = create_array2d(board_length, board_length);
+populate_array2d(array2d, board_length, board_length);
+render(array2d);
